a35c7d07005a961bf708a33a77ea263869a9328e
GabrielHS <gabrielhenri0012@gmail.com>
2022-07-12 11:33:11 -0300

    feat: initial base config

---
+++ src/main/java/com/redandmoon/time/thisisme/ThisIsMeApplication.java
@@ -0,0 +1,13 @@
+package com.redandmoon.time.thisisme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThisIsMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThisIsMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redandmoon/time/thisisme/ThisIsMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redandmoon.time.thisisme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThisIsMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
